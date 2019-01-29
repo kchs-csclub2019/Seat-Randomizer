0dcd7aa727c6df30f1ceb967bc490db779cfd8e9
Choi <iamchoij@gmail.com>
2019-01-29 17:07:01 -0600

    Window size changed

--- src/GUISetup.java
+++ src/GUISetup.java
@@ -6,17 +6,25 @@ public class GUISetup extends JFrame
     // window variable
     private Container window;
 
+    private int screenHeight = (int)(java.awt.Toolkit.getDefaultToolkit().getScreenSize().getHeight());
+    private int screenWidth = (int)(java.awt.Toolkit.getDefaultToolkit().getScreenSize().getWidth());
+
+    private int sizeX = (screenWidth * 3) / 4;
+    private int sizeY = (screenHeight * 3) / 4;
+
+    //
+
+
     public void makeTheWindow()
     {
+
         window = getContentPane();
         window.setLayout(null);
         window.setBackground(Color.WHITE);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Seat Randomizer");
-        int screenHeight = (int)(java.awt.Toolkit.getDefaultToolkit().getScreenSize().getHeight());
-        int screenWidth = (int)(java.awt.Toolkit.getDefaultToolkit().getScreenSize().getWidth());
-        setSize(800, 600);
-        setLocation(screenWidth / 2 - 800 / 2, screenHeight / 2 - 800 / 2);
+        setSize(sizeX, sizeY);
+        setLocation(screenWidth / 2 - sizeX / 2, screenHeight / 2 - sizeY / 2);
         setResizable(false);
     }
 }
--- src/SeatRandomizer.java
+++ src/SeatRandomizer.java
@@ -3,5 +3,6 @@ public class SeatRandomizer
     public static void main(String[] args) {
         GUISetup window = new GUISetup();
         window.makeTheWindow();
+        window.setVisible(true);
     }
 }
