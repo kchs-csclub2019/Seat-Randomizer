f128da28dfded2d8b2ca223dcceb16c4d0bf2d69
Jeongwoo55 <iamchoij@gmail.com>
2019-02-07 14:48:49 -0600

    Update readFiles.java

--- src/readFiles.java
+++ src/readFiles.java
@@ -1,6 +1,24 @@
 public class readFiles
 {
+	private String className;
+	private Scanner file;
 	public readFiles
+	{
+		
+	}
+	
+	public readFiles(String s)
+	{
+		setClassName(s);
+	}
+	
+	public void setClassName(String s)
+	{
+		className = s;
+		file = Scanner(new File(className + ".dat"));
+	}
+	
+	public String[] getNames
 	{
 		
 	}
